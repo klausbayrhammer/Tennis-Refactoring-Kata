6f35d806a22146663b2136fee1785682a5856f98
Klaus Bayrhammer <bayrhammer.klaus@gmail.com>
2016-10-04 19:06:09 +0200

    splits functions, replace switches with configs

--- javascript/TennisGame1.js
+++ javascript/TennisGame1.js
@@ -1,65 +1,69 @@
-var TennisGame1 = function() {
+var TennisGame1 = function () {
     this.scorePlayerOne = 0;
     this.scorePlayerTwo = 0;
 };
 
-TennisGame1.prototype.wonPoint = function(playerName) {
-    if (playerName === "player1")
+
+TennisGame1.prototype.wonPoint = function (playerName) {
+    if (playerName === "player1") {
         this.scorePlayerOne++;
-    else
-        this.scorePlayerTwo++;
-};
-
-TennisGame1.prototype.getScore = function() {
-    var score = "";
-    var tempScore = 0;
-    if (this.scorePlayerOne === this.scorePlayerTwo) {
-        switch (this.scorePlayerOne) {
-            case 0:
-                score = "Love-All";
-                break;
-            case 1:
-                score = "Fifteen-All";
-                break;
-            case 2:
-                score = "Thirty-All";
-                break;
-            default:
-                score = "Deuce";
-                break;
-        }
-    } else if (this.scorePlayerOne >= 4 || this.scorePlayerTwo >= 4) {
-        var minusResult = this.scorePlayerOne - this.scorePlayerTwo;
-        if (minusResult === 1) score = "Advantage player1";
-        else if (minusResult === -1) score = "Advantage player2";
-        else if (minusResult >= 2) score = "Win for player1";
-        else score = "Win for player2";
     } else {
-        for (var i = 1; i < 3; i++) {
-            if (i === 1) tempScore = this.scorePlayerOne;
-            else {
-                score += "-";
-                tempScore = this.scorePlayerTwo;
-            }
-            switch (tempScore) {
-                case 0:
-                    score += "Love";
-                    break;
-                case 1:
-                    score += "Fifteen";
-                    break;
-                case 2:
-                    score += "Thirty";
-                    break;
-                case 3:
-                    score += "Forty";
-                    break;
-            }
-        }
+        this.scorePlayerTwo++;
     }
-    return score;
 };
 
-if (typeof window === "undefined") {
-    module.exports = TennisGame1;
+
+function getDrawScore(score) {
+    const DRAW_SCORE = {
+        0: "Love-All",
+        1: "Fifteen-All",
+        2: "Thirty-All",
+        deuce: "Deuce"
+    };
+    return DRAW_SCORE[score] || DRAW_SCORE.deuce;
 }
+
+
+function getAdvantageGameScore(scorePlayerOne, scorePlayerTwo) {
+    const minusResult = scorePlayerOne - scorePlayerTwo;
+    if (minusResult === 1)
+        return "Advantage player1";
+    else if (minusResult === -1)
+        return "Advantage player2";
+    else if (minusResult >= 2)
+        return "Win for player1";
+    else
+        return "Win for player2";
+
+}
+
+function getSinglePlayerStandardScore(tempScore) {
+    const SINGLE_PLAYER_SCORE = {
+        0: "Love",
+        1: "Fifteen",
+        2: "Thirty",
+        3: "Forty"
+    };
+    return SINGLE_PLAYER_SCORE[tempScore];
+}
+
+function getStandardScore(scorePlayerOne, scorePlayerTwo) {
+    const playerOneScoreDescription = getSinglePlayerStandardScore(scorePlayerOne);
+    const playerTwoScoreDescription = getSinglePlayerStandardScore(scorePlayerTwo);
+    return `${playerOneScoreDescription}-${playerTwoScoreDescription}`;
+}
+
+TennisGame1.prototype.getScore = function () {
+    const isDrawScore = this.scorePlayerOne === this.scorePlayerTwo;
+    const isAdvantageGame = this.scorePlayerOne >= 4 || this.scorePlayerTwo >= 4;
+
+    if (isDrawScore) {
+        return getDrawScore(this.scorePlayerOne);
+    } else if (isAdvantageGame) {
+        return getAdvantageGameScore(this.scorePlayerOne, this.scorePlayerTwo)
+    } else {
+        return getStandardScore(this.scorePlayerOne, this.scorePlayerTwo);
+    }
+};
+
+module.exports = TennisGame1;
